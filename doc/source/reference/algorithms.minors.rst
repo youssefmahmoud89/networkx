cbf88aaff39ae9247eec426d4a416e759667a15b
yousefma7moud <youssef.youssef@smail.inf.h-brs.de>
2019-05-01 12:32:40 +0200

    added networkx files

---
+++ doc/source/reference/algorithms.minors.rst
@@ -0,0 +1,11 @@
+******
+Minors
+******
+
+.. automodule:: networkx.algorithms.minors
+.. autosummary::
+   :toctree: generated/
+
+   contracted_edge
+   contracted_nodes
+   quotient_graph
---
+++ networkx/version.py
@@ -0,0 +1,25 @@
+"""
+Version information for NetworkX, created during installation.
+
+Do not add this file to the repository.
+
+"""
+
+import datetime
+
+version = '2.0.dev_20150527162450'
+date = 'Wed May 27 16:24:50 2015'
+
+# Was NetworkX built from a development version? If so, remember that the major
+# and minor versions reference the "target" (rather than "current") release.
+dev = True
+
+# Format: (name, major, min, revision)
+version_info = ('networkx', '2', '0', None)
+
+# Format: a 'datetime.datetime' instance
+date_info = datetime.datetime(2015, 5, 27, 16, 24, 50, 849043)
+
+# Format: (vcs, vcs_tuple)
+vcs_info = (None, (None, None))
+
